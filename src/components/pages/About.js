f038fc2baccb4d69c07581f32f152fd7b33952bd
andiraco <34205178+andiraco@users.noreply.github.com>
2020-03-18 16:06:00 -0600

    initial commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function About(){
+    
+        return (
+            <React.Fragment>
+                <h1>About</h1>
+                <p>This is the TodoList app V1.0.0 It is part of 
+                    React crash course
+                </p>
+            </React.Fragment>
+        )
+    
+}
