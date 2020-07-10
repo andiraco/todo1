16cf405c2331e8de3fe132e2186d8dedddf229e8
andiraco <34205178+andiraco@users.noreply.github.com>
2020-07-09 23:50:46 -0600

    Updates

---
+++ precache-manifest.a924b6ae653a359723013e84548bc4b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bc4d6e067bc7c2a1204c4f32817e386",
+    "url": "/todo1/index.html"
+  },
+  {
+    "revision": "1e1f2ad56e4b284487a1",
+    "url": "/todo1/static/css/main.c3234951.chunk.css"
+  },
+  {
+    "revision": "bc99315bb75405d2ecc8",
+    "url": "/todo1/static/js/2.161cc9b5.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/todo1/static/js/2.161cc9b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e1f2ad56e4b284487a1",
+    "url": "/todo1/static/js/main.3636c553.chunk.js"
+  },
+  {
+    "revision": "55dbad38a4a8d3ef8f13",
+    "url": "/todo1/static/js/runtime-main.5397daf7.js"
+  }
+]);
